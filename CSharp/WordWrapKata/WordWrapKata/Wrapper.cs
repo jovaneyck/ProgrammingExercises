1643c7e381bc5036307cd489b2004b0aac506e4c
Jo Van Eyck <jo.vaneyck@gmail.com>
2013-01-10 18:14:33 +0100

    Refactored: names reflect the meaning of the variable again

--- CSharp/WordWrapKata/WordWrapKata/Wrapper.cs
+++ CSharp/WordWrapKata/WordWrapKata/Wrapper.cs
@@ -7,10 +7,11 @@ namespace WordWrapKata
             if (stringToWrap.Length <= columnLength)
                 return stringToWrap;
 
-            int firstWhiteSpaceLocation = stringToWrap.LastIndexOf(' ', columnLength);
+            int lastUsefullWhiteSpaceLocation = stringToWrap.LastIndexOf(' ', columnLength);
             int indexToWrapAt;
-            if (firstWhiteSpaceLocation != -1 && firstWhiteSpaceLocation < columnLength)
-                indexToWrapAt = firstWhiteSpaceLocation;
+
+            if (lastUsefullWhiteSpaceLocation != -1 && lastUsefullWhiteSpaceLocation < columnLength)
+                indexToWrapAt = lastUsefullWhiteSpaceLocation;
             else
             {
                 indexToWrapAt = columnLength;
@@ -18,8 +19,6 @@ namespace WordWrapKata
 
             string firstPart = stringToWrap.Substring(0, indexToWrapAt);
             string secondPart = stringToWrap.Substring(indexToWrapAt, stringToWrap.Length - indexToWrapAt).Trim(); 
-
-
 
             return firstPart + "\n" + wrap(secondPart, columnLength);
         }
