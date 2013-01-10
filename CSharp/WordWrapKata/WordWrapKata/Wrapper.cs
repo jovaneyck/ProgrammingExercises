8b3fb122c73614be586a8eda7f433fcb2b28951d
Jo Van Eyck <jo.vaneyck@gmail.com>
2013-01-10 17:36:45 +0100

    Pass the whitespace split test

--- CSharp/WordWrapKata/WordWrapKata/Wrapper.cs
+++ CSharp/WordWrapKata/WordWrapKata/Wrapper.cs
@@ -10,8 +10,22 @@ namespace WordWrapKata
             }
             else
             {
-                var firstPart = stringToWrap.Substring(0, columnLength);
-                string secondPart = stringToWrap.Substring(columnLength, stringToWrap.Length - columnLength).Trim();
+                int firstWhiteSpaceLocation = stringToWrap.IndexOf(" ");
+                string firstPart;
+                string secondPart;
+
+                if(firstWhiteSpaceLocation != -1 && firstWhiteSpaceLocation < columnLength)
+                {
+                    firstPart = stringToWrap.Substring(0, firstWhiteSpaceLocation);
+                    secondPart = stringToWrap.Substring(firstWhiteSpaceLocation, stringToWrap.Length - firstWhiteSpaceLocation).Trim();
+                }
+                else
+                {
+                    firstPart = stringToWrap.Substring(0, columnLength);
+                    secondPart = stringToWrap.Substring(columnLength, stringToWrap.Length - columnLength).Trim(); 
+                }
+
+
                 return firstPart + "\n" + wrap(secondPart, columnLength);
             }
         }
