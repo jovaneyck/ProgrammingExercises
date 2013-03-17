6768e4993055d58d9305e460fe6e77796765072e
Jo Van Eyck <jo.vaneyck@gmail.com>
2013-03-17 15:01:53 +0100

    Added a visual acceptance test. Basic requirements complete.

--- CSharp/FizzBuzz/FizzBuzz/Program.cs
+++ CSharp/FizzBuzz/FizzBuzz/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FizzBuzz
 {
@@ -10,6 +6,11 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            FizzBuzz buzzer = new FizzBuzz();
+
+            for(int number = 1; number <= 100; number++)
+                Console.WriteLine(buzzer.FizzBuzzify(number));
+            Console.ReadKey();
         }
     }
 }
