20300d9ce5dc7a11164930c577185742020ab972
Jo Van Eyck <jo.vaneyck@gmail.com>
2013-03-17 14:37:36 +0100

    setup infrastructure + first passing test

---
+++ CSharp/FizzBuzz/FizzBuzz/FizzBuzzerTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace FizzBuzz
+{
+    [TestFixture]
+    class FizzBuzzerTest
+    {
+        [Test]
+        public void TransformsOneCorrectly()
+        {
+            const int number = 1;
+            Assert.AreEqual(number, FizzBuzzify(number));
+        }
+
+        private int FizzBuzzify(int number)
+        {
+            return 1;
+        }
+    }
+}
