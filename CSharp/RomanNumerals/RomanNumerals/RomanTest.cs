1aed1de484882847592b33be063585411905a49d
Jo Van Eyck <jo.vaneyck@gmail.com>
2013-03-23 11:06:50 +0100

    Project setup. Added case for I

---
+++ CSharp/RomanNumerals/RomanNumerals/RomanTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace RomanNumerals
+{
+    [TestFixture]
+    class RomanTest
+    {
+        [Test]
+        public void One()
+        {
+            Assert.AreEqual("I", ToRoman(1));
+        }
+
+        private string ToRoman(int i)
+        {
+            return "I";
+        }
+    }
+}
