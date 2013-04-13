4d04ae02a7ca50a21ea7f89d585a792231d6b7b7
Jo Van Eyck <jo.vaneyck@gmail.com>
2013-04-13 16:47:00 +0200

    quality degrades twice as fast when sellin has expired

--- CSharp/GildedRoseKata/GildedRoseKata/GildedRoseTest.cs
+++ CSharp/GildedRoseKata/GildedRoseKata/GildedRoseTest.cs
@@ -6,6 +6,9 @@ namespace GildedRoseKata
     [TestFixture]
     public class GildedRoseTest
     {
+        private const int SellInFactor = 1;
+        private const int QualityFactor = 1;
+
         [Test]
         public void UpdateQualityDoesNotChangeName()
         {
@@ -36,21 +39,32 @@ namespace GildedRoseKata
         }
 
         [Test]
-        public void SellInDecreasesByOne()
+        public void SellInDecreasesByFactor()
         {
             const int initialSellIn = 100;
             Item item = CreateAndAdvanceSingleItem("item", initialSellIn, 100);
 
-            Assert.AreEqual(item.SellIn, initialSellIn - 1);
+            Assert.AreEqual(initialSellIn - SellInFactor, item.SellIn);
         }
 
         [Test]
-        public void QualityDecreasesByOne()
+        public void QualityDecreasesByFactor()
         {
             const int initialQuality = 100;
             Item item = CreateAndAdvanceSingleItem("item", 100, initialQuality);
 
-            Assert.AreEqual(item.Quality, initialQuality - 1);
+            Assert.AreEqual(initialQuality - QualityFactor, item.Quality);
+        }
+
+        [Test]
+        public void WhenSellInHasPassedQualityDegradesTwiceAsFast()
+        {
+            const int initialQuality = 100;
+            const int expiredSellIn = 0;
+
+            Item item = CreateAndAdvanceSingleItem("item", expiredSellIn, initialQuality);
+
+            Assert.AreEqual(initialQuality - 2*QualityFactor, item.Quality);
         }
 
 
