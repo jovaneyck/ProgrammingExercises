aad39f6f5df7d3168cf9f8e761dd693e34a8b887
Jo Van Eyck <jo.vaneyck@gmail.com>
2013-04-13 16:33:49 +0200

    added test: the update method lowers both quality and sellin for a random item

--- CSharp/GildedRoseKata/GildedRoseKata/GildedRoseTest.cs
+++ CSharp/GildedRoseKata/GildedRoseKata/GildedRoseTest.cs
@@ -11,9 +11,26 @@ namespace GildedRoseKata
         {
             const string uniqueName = "uniqueName";
             IList<Item> items = new List<Item> {new Item {Name = uniqueName, SellIn = 0, Quality = 0}};
-            var app = new GildedRose(items);
+            GildedRose app = new GildedRose(items);
             app.UpdateQuality();
             Assert.AreEqual(uniqueName, items[0].Name);
+        } 
+        
+        [Test]
+        public void AdvancingADayLowersBothQualityAndSellIn()
+        {
+            const int initialSellIn = 100;
+            const int initialQuality = 100;
+            IList<Item> items = new List<Item> {new Item {Name = "item1", SellIn = initialSellIn, Quality = initialQuality}};
+
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            
+            Assert.IsTrue(items[0].SellIn < initialSellIn);
+            Assert.IsTrue(items[0].Quality < initialQuality);
         }
+
+
+
     }
 }
