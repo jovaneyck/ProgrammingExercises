a83cdb9cbcc4d8a210e1ea3694a8fee8b739b33a
Jo Van Eyck <jo.vaneyck@gmail.com>
2013-05-30 15:41:28 +0200

    added a golden master test. Feels a bit silly though, since the sensor always returns the same value

---
+++ CSharp/TDDwithMockObjectsAndDesignPrinciples/TDDMicroExercises/TirePressureMonitoringSystem.Test/GoldenMasterTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+
+namespace TDDMicroExercises.TirePressureMonitoringSystem.Test
+{
+    [TestFixture]
+    public class GoldenMasterTest
+    {
+        [Test]
+        public void NUnitSanityCheck()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        [UseReporter(typeof(NUnitReporter))]
+        public void AlarmUsedInRealisticScenario()
+        {
+            //Note that this is a VERY high-level regression test: 
+            // the dummy sensor always returns the same value, so the alarm will always be on after the first pressure check.
+            StringBuilder s = new StringBuilder();
+            for(int i = 0; i < 100; i++)
+            {
+                Alarm a = new Alarm();
+                for(int j = 0; j < 10; j++)
+                {
+                    s.AppendLine(String.Format("Run {0} {1}: {2}", i,j, a.AlarmOn));
+                    a.Check();
+                }
+            }
+
+            Approvals.Verify(s);
+        }
+    }
+}
