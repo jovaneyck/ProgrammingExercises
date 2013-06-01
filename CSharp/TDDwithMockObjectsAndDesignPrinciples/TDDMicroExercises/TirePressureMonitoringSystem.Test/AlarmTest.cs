1a85c7bb244f64e5725022ab6430323edb89ee4a
Jo Van Eyck <jo.vaneyck@gmail.com>
2013-06-01 14:42:44 +0200

    check if alarm remembers being triggered

--- CSharp/TDDwithMockObjectsAndDesignPrinciples/TDDMicroExercises/TirePressureMonitoringSystem.Test/AlarmTest.cs
+++ CSharp/TDDwithMockObjectsAndDesignPrinciples/TDDMicroExercises/TirePressureMonitoringSystem.Test/AlarmTest.cs
@@ -8,6 +8,9 @@ namespace TDDMicroExercises.TirePressureMonitoringSystem.Test
     {
         private Sensor _sensorImpl;
         private Alarm alarm;
+        private const double _underLowerTreshold = 1d;
+        private const double _betweenTresholds = 20d;
+        private const double _aboveUpperTreshold = 22d;
 
         [SetUp]
         public void SetUp()
@@ -25,21 +28,47 @@ namespace TDDMicroExercises.TirePressureMonitoringSystem.Test
         [Test]
         public void TriggersWhenBelowTreshold()
         {
-            AssertAlertTriggersFor(1d);
+            AssertAlertTriggersFor(_underLowerTreshold);
         }
 
         private void AssertAlertTriggersFor(double sensorValue)
         {
-            _sensorImpl.Stub(s => s.PopNextPressurePsiValue()).Return(sensorValue);
-
-            alarm.Check();
+            SetupSensorAndCheckAlarm(sensorValue);
             Assert.IsTrue(alarm.AlarmOn);
         }
-        
+
+        private void SetupSensorAndCheckAlarm(double sensorValue)
+        {
+            _sensorImpl.Stub(s => s.PopNextPressurePsiValue()).Return(sensorValue);
+            alarm.Check();
+        }
+
         [Test]
         public void TriggersWhenAboveTreshold()
         {
-            AssertAlertTriggersFor(22d);
+            AssertAlertTriggersFor(_aboveUpperTreshold);
+        }
+
+        [Test]
+        public void DoesNotTriggerBetweenBoundaries()
+        {
+            AssertAlertDoesNotTriggerFor(_betweenTresholds);
+        }
+
+        private void AssertAlertDoesNotTriggerFor(double sensorValue)
+        {
+            SetupSensorAndCheckAlarm(sensorValue);
+            Assert.IsFalse(alarm.AlarmOn);
+        }
+
+        [Test]
+        public void RemembersWhenTresholdWasBreached()
+        {
+            SetupSensorAndCheckAlarm(_underLowerTreshold);
+            Assert.IsTrue(alarm.AlarmOn);
+
+            SetupSensorAndCheckAlarm(_betweenTresholds);
+            Assert.IsTrue(alarm.AlarmOn);
         }
     }
 }
