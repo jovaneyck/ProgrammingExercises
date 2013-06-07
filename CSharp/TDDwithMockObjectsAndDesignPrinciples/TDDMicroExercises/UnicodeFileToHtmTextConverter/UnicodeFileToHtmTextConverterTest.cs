3b920b586b2c8e50addb51060587558b583a8c0f
Jo Van Eyck <jo.vaneyck@gmail.com>
2013-06-07 16:15:51 +0200

    Added a golden master test to make sure we don't break anythingé

---
+++ CSharp/TDDwithMockObjectsAndDesignPrinciples/TDDMicroExercises/UnicodeFileToHtmTextConverter/UnicodeFileToHtmTextConverterTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+
+namespace TDDMicroExercises.UnicodeFileToHtmTextConverter
+{
+    [TestFixture]
+    class UnicodeFileToHtmTextConverterTest
+    {
+        [Test]
+        [UseReporter(typeof(NUnitReporter))]
+        public void ConvertsAFileToHtml()
+        {
+            //This test hits the filesystem AND needs a real file for every test run :(
+            //Hardcoded dependency on the File API is a violiation of SoliD:
+            //Multiple repsonsibilities: reading a file from disk AND converting it to HTML
+            //Hardwired dependency on a concrete type, rather than an abstraction: File API.
+            const string fullFileName = "./UnicodeFileToHtmTextConverter/testFile.txt";
+            UnicodeFileToHtmTextConverter converter = new UnicodeFileToHtmTextConverter(fullFileName);
+            string result = converter.ConvertToHtml();
+           Approvals.Verify(result);
+        }
+    }
+}
