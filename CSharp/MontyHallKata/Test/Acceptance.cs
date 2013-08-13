dc48bd26b524dccdb213ad5c68a88e7a1387eee8
Jo Van Eyck <jo.vaneyck@gmail.com>
2013-08-13 16:09:44 +0200

    Made the acceptance test deterministic. Not happy with how I did it, i.e. passing in random numbers as a command-line argument, but for practice code it will do.

--- CSharp/MontyHallKata/Test/Acceptance.cs
+++ CSharp/MontyHallKata/Test/Acceptance.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using NUnit.Framework;
 using TestStack.White;
 using TestStack.White.Factory;
@@ -12,17 +14,7 @@ namespace Test
     {
         private Application application;
         private Window mainWindow;
-
-        [SetUp]
-        public void SetUp()
-        {
-            //As there is no straightforward way of replacing the dependency of a random number generator using plain White, we are working with a fixed seed.
-            //Note that this can be achieved by spawning another thread which programmatically starts the application, this is somehting I should look into before starting the real implementation!
-            const string commandLineArguments = "seed:1337";
-            ProcessStartInfo startInfo = new ProcessStartInfo("MontyHallKata.exe", commandLineArguments);
-            application = Application.Launch(startInfo);
-            mainWindow = application.GetWindow("MainWindow", InitializeOption.NoCache);
-        }
+        private const int NumberOfExperiments = 10000;
 
         [TearDown]
         public void TearDown()
@@ -34,15 +26,62 @@ namespace Test
         [Test]
         public void RendersResultsOfASingleSimulation()
         {
-           StartASingleSimulation();
+            StartApplicationWithRandomNumbers(GenerateRandomNumbers(0));
+            StartASingleSimulation();
             AssertSingleExperimentHasASuccessfullOutcome();
+        }
+
+        private List<int> GenerateRandomNumbers(int value)
+        {
+            List<int> randomNumbers = new List<int>();
+
+            for (int i = 0; i < NumberOfExperiments; i++)
+            {
+                randomNumbers.Add(value);
+            }
+
+            return randomNumbers;
+        }
+
+        public void StartApplicationWithRandomNumbers(List<int> randomNumbers)
+        {
+            //Pass in the random numbers as a command-line argument. This is NOT something I would ever do in a real application :(
+            var randomNumbersAsString = RandomNumbersAsString(randomNumbers);
+
+            string commandLineArguments = String.Format("rng:{0}", randomNumbersAsString);
+            ProcessStartInfo startInfo = new ProcessStartInfo("MontyHallKata.exe", commandLineArguments);
+            application = Application.Launch(startInfo);
+            mainWindow = application.GetWindow("MainWindow", InitializeOption.NoCache);
+        }
+
+        private string RandomNumbersAsString(IEnumerable<int> randomNumbers)
+        {
+            string randomNumbersAsString = "";
+
+            foreach (int number in randomNumbers)
+            {
+                if (!randomNumbersAsString.Equals(String.Empty))
+                    randomNumbersAsString += ", ";
+
+                randomNumbersAsString += number;
+            }
+            return randomNumbersAsString;
         }
 
         private void StartASingleSimulation()
         {
-            TextBox numberOfExperimentsInput = mainWindow.Get<TextBox>("numberOfExperiments");
-            numberOfExperimentsInput.Text = "1";
+            SetNumberOfExperiments(1);
+            RunSimulations();
+        }
 
+        private void SetNumberOfExperiments(int numberOfExperiments)
+        {
+            TextBox numberOfExperimentsInput = mainWindow.Get<TextBox>("numberOfExperiments");
+            numberOfExperimentsInput.Text = numberOfExperiments.ToString();
+        }
+
+        private void RunSimulations()
+        {
             Button startSimulationButton = mainWindow.Get<Button>("startSimulation");
             startSimulationButton.Click();
         }
