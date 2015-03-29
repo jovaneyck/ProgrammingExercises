f30d0d8948b09d923c2076ee421a537219d1a02b
Jo Van Eyck <jo.vaneyck@gmail.com>
2015-03-29 13:53:40 +0200

    JB is right, valuesByKey is a nice mechanical naming convention for dictionaries

--- CSharp/PointOfSale/PointOfSale/PriceRegistry.cs
+++ CSharp/PointOfSale/PointOfSale/PriceRegistry.cs
@@ -4,22 +4,22 @@ namespace PointOfSale
 {
     public class PriceRegistry
     {
-        private readonly Dictionary<Barcode, Price> _priceForBarcode = new Dictionary<Barcode, Price>(); 
+        private readonly Dictionary<Barcode, Price> _pricesForBarcode = new Dictionary<Barcode, Price>(); 
 
         public Price PriceOf(Barcode barcode)
         {
-            return _priceForBarcode[barcode];
+            return _pricesForBarcode[barcode];
         }
 
         public PriceRegistry Register(Barcode barcode, Price price)
         {
-            _priceForBarcode.Add(barcode, price);
+            _pricesForBarcode.Add(barcode, price);
             return this;
         }
 
         public bool HasPriceFor(string barcode)
         {
-            return _priceForBarcode.ContainsKey(barcode);
+            return _pricesForBarcode.ContainsKey(barcode);
         }
     }
 }
