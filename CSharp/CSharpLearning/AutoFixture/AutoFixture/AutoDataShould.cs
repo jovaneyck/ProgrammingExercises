b78787de2fc2efa83cf8d8dd1434a36fe919850d
Jo Van Eyck <jo.vaneyck@gmail.com>
2015-05-10 15:59:26 +0200

    automocking

---
+++ CSharp/CSharpLearning/AutoFixture/AutoFixture/AutoDataShould.cs
@@ -0,0 +1,46 @@
+﻿using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace AutoFixture
+{
+    public class AutoDataShould
+    {
+        [Theory]
+        [InlineData(1, 2)]
+        public void SupportParametrizedTestsXUnitStyle(int input, int expected)
+        {
+            Assert.Equal(input + 1, expected);
+        }
+
+        public class Adder
+        {
+            public int Add(int a, int b)
+            {
+                return a + b;
+            } 
+        }
+
+        [Theory]
+        [AutoData]
+        public void ProvideDataAutomatically(int first, int second) //autofixture provides randomly created instances of input arguments
+        {
+            Assert.Equal(first+second, new Adder().Add(first, second));
+        }
+
+        [Theory]
+        [AutoData]
+        public void ProvideTheSUT(Adder adder, int first, int second) //the SUT can also be automagically instantiated using default fixture creation.
+        {
+            Assert.Equal(first + second, adder.Add(first, second));
+        }
+
+        [Theory]
+        [InlineAutoData] //none provided
+        [InlineAutoData(1,2)] //inputs provided
+        [InlineAutoData(1)] //partial input provided
+        public void MixAndMatchSpecificallyProvidedAndAutoCreatedInput(int first, int second, Adder adder)
+        {
+            Assert.Equal(first + second, adder.Add(first, second));
+        }
+    }
+}
---
+++ CSharp/CSharpLearning/AutoFixture/AutoFixture/AutoMockingShould.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Moq;
+using Ploeh.AutoFixture;
+using System.Collections.Generic;
+using Ploeh.AutoFixture.AutoMoq;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace AutoFixture
+{
+    public class AutoMockingShould
+    {
+        [Fact]
+        public void ManualCreatedSUTWithMockedCollaborator()
+        {
+            //how we usually do it
+            var fixture = new Fixture();
+            var gateway = new Mock<EmailGateway>();
+            var sut = new EmailBuffer(gateway.Object);
+            sut.Add(fixture.Create<EmailMessage>());
+
+            sut.Send();
+
+            gateway.Verify(g=>g.Send(It.IsAny<EmailMessage>()));
+        }
+
+        [Fact]
+        public void CannotByDefaultUseAutoFixtureToResolveAbstractDependencies()
+        {
+            var fixture = new Fixture();
+
+            Assert.Throws<ObjectCreationException>(() => fixture.Create<EmailBuffer>()); //has dependency to abstract EmailGateway type.
+        }
+
+        [Fact]
+        public void TurnIntoAnAutoMockingContainer()
+        {
+            var fixture = new Fixture();
+            fixture.Customize(new AutoMoqCustomization());
+
+            var sut = fixture.Create<EmailBuffer>();
+
+        }
+
+        [Fact]
+        public void ProvideMockedCollaboratorsUsingFreeze()
+        {
+            var fixture = new Fixture();
+            fixture.Customize(new AutoMoqCustomization());
+            var gateway = fixture.Freeze<Mock<EmailGateway>>(); //get mock, same mock will be returned in all future requests.
+            var sut = fixture.Create<EmailBuffer>();
+            sut.Add(fixture.Create<EmailMessage>());
+
+            sut.Send();
+
+            gateway.Verify(g=>g.Send(It.IsAny<EmailMessage>()));
+
+        }
+
+        [Fact]
+        public void AcceptAlreadyCreatedMockCollaboratorsUsingInject()
+        {
+            var fixture = new Fixture();
+            fixture.Customize(new AutoMoqCustomization());
+            var gateway = new Mock<EmailGateway>();
+            fixture.Inject(gateway); //Inject an already created object, will be used in all future requests for that type.
+
+            var sut = fixture.Create<EmailBuffer>();
+            sut.Add(fixture.Create<EmailMessage>());
+
+            sut.Send();
+
+            gateway.Verify(g=>g.Send(It.IsAny<EmailMessage>()));
+
+        }
+
+        [Theory]
+        [AutoMoqData]
+        public void AutoMockWithoutAnySetupByUsingACustomAttribute(
+            [Frozen] Mock<EmailGateway> gateway, //Frozen attribute freezes a type
+            //Note that the ORDER OF ARGUMENTS MATTERS HERE. if not frozen before a buffer object is created, gateway is not frozen yet!!!
+            EmailBuffer buffer, 
+            EmailMessage message
+            )
+        {
+            buffer.Add(message);
+            buffer.Send();
+
+            gateway.Verify(g=>g.Send(It.IsAny<EmailMessage>()));
+        }
+    }
+
+    public class AutoMoqDataAttribute : AutoDataAttribute
+    {
+        public AutoMoqDataAttribute()
+            : base(new Fixture().Customize(new AutoMoqCustomization())) //Boom!
+        {
+        }
+    }
+
+    public class EmailBuffer
+    {
+        private readonly List<EmailMessage> _bufferedMessages = new List<EmailMessage>();
+        private readonly EmailGateway _gateway;
+
+        public EmailBuffer(EmailGateway gateway)
+        {
+            _gateway = gateway;
+        }
+
+        public void Add(EmailMessage message)
+        {
+            _bufferedMessages.Add(message);
+        }
+
+        public void Send()
+        {
+            foreach(var msg in _bufferedMessages)
+            {
+                _gateway.Send(msg);
+            }
+
+            _bufferedMessages.Clear();
+        }
+    }
+
+    public interface EmailGateway
+    {
+        void Send(EmailMessage msg);
+    }
+
+    public class EmailMessage
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
