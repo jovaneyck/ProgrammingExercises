e39b76347a95f3ffa23d814cdadd84a1a9407813
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-01-13 09:15:28 +0100

    did switcheroo kata

---
+++ CSharp/CodeWars/CodeWars/TheOldSwitcheroo/Kata.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using static System.Char;
+
+namespace CodeWars.TheOldSwitcheroo
+{
+    public class Kata
+    {
+        public static string Encode(string str)
+        {
+            return
+                string.Join(
+                    "",
+                    str
+                        .ToCharArray()
+                        .SelectMany(Translate));
+
+        }
+
+        private static string Translate(char c)
+        {
+            if (!IsLetter(c))
+            {
+                return c.ToString();
+            }
+
+            const int charOffset = 96;
+            return (ToLower(c) - charOffset).ToString();
+        }
+    }
+}
---
+++ CSharp/CodeWars/CodeWars/TheOldSwitcheroo/Test.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.TheOldSwitcheroo
+{
+    public class Test
+    {
+        [Test]
+        [TestCase("abc", "123")]
+        [TestCase("ABCD", "1234")]
+        [TestCase("ZzzzZ", "2626262626")]
+        [TestCase("abc-#@5", "123-#@5")]
+        [TestCase("this is a long string!! Please [encode] @C0RrEctly", "208919 919 1 1215147 1920189147!! 161251195 [51431545] @30181853201225")]
+        public static void FixedTest(string str, string expected)
+        {
+            Assert.AreEqual(expected, Kata.Encode(str));
+        }
+    }
+}
