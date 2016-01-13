e3b489e034f80cb4edf4cc0e84bf27cf19801589
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-01-13 09:01:40 +0100

    first codewars kata done. Super fun!

---
+++ CSharp/CodeWars/CodeWars/CountConsonants/Kata.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using static System.Char;
+
+namespace CodeWars.CountConsonants
+{
+    public class Kata
+    {
+        private static readonly string Vowels = "bcdfghjklmnpqrstvwxyz";
+
+        public static int ConsonantCount(string str)
+        { 
+            return str
+                .ToCharArray()
+                .Select(ToLower)
+                .Count(c => Vowels.Contains(c));
+        }
+    }
+}
---
+++ CSharp/CodeWars/CodeWars/CountConsonants/Tests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.CountConsonants
+{
+
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        [TestCase("", 0)]
+        [TestCase("aaaaa", 0)]
+        [TestCase("Bbbbb", 5)]
+        [TestCase("helLo world", 7)]
+        [TestCase("h^$&^#$&^elLo world", 7)]
+        [TestCase("012456789", 0)]
+        [TestCase("012345_Cb", 2)]
+        public static void FixedTest(string s, int result)
+        {
+            Assert.AreEqual(result, Kata.ConsonantCount(s));
+        }
+    }
+}
