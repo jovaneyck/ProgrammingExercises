60431a98f10596406e280cdcc4f6966c03ad47b8
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-01-14 08:46:27 +0100

    finished area of circle

---
+++ CSharp/CodeWars/CodeWars/AreaOfACircle/Kata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using static System.Double;
+
+namespace CodeWars.AreaOfACircle
+{
+    public class Kata
+    {
+        public static double CalculateAreaOfCircle(string radius)
+        {
+            double radiusAsNumber;
+            if (! TryParse(radius, out radiusAsNumber) || radiusAsNumber <= 0)
+            {
+                throw new ArgumentException();
+            }
+
+            return 
+                Math.Round(
+                    Math.PI * Math.Pow(radiusAsNumber, 2),
+                    2);
+        }
+
+        private static bool TryParse(string radius, out double radiusAsNumber)
+        {
+            return double.TryParse(
+                radius, 
+                NumberStyles.AllowDecimalPoint, 
+                new NumberFormatInfo {NumberDecimalSeparator = "."}, 
+                out radiusAsNumber);
+        }
+    }
+}
---
+++ CSharp/CodeWars/CodeWars/AreaOfACircle/Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CodeWars.AreaOfACircle
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void ShouldFailNegativeNumber()
+        {
+            Assert.Throws<ArgumentException>(delegate { Kata.CalculateAreaOfCircle("-123"); });
+        }
+
+        [Test]
+        public void ShouldFailAlphaNumeric()
+        {
+            Assert.Throws<ArgumentException>(delegate { Kata.CalculateAreaOfCircle("number"); });
+        }
+
+        [Test]
+        public void ShouldPass1()
+        {
+            Assert.AreEqual(5881.25, Kata.CalculateAreaOfCircle("43.2673"));
+        }
+    }
+}
