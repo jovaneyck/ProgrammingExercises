4e08d76802dd233dacbd383a9d838ba185b08ab2
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-01-16 16:45:35 +0100

    First class function factory

---
+++ CSharp/CodeWars/CodeWars/FirstClassFunctionFactory/FunctionFactory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+namespace CodeWars.FirstClassFunctionFactory
+{
+    public class FunctionFactory
+    {
+        public static Func<int[], int[]> factory(int multiplier) =>
+            numbers => numbers.Select(n => n * multiplier).ToArray();
+    }
+}
---
+++ CSharp/CodeWars/CodeWars/FirstClassFunctionFactory/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CodeWars.FirstClassFunctionFactory
+{
+    [TestFixture]
+    public class FactoryTest
+    {
+        [Test]
+        public static void BasicTest()
+        {
+            var threes = FunctionFactory.factory(3);
+            int[] myArr = { 1, 2, 3 };
+            Assert.AreEqual(threes(myArr), new [] { 3, 6, 9 });
+            var fives = FunctionFactory.factory(5);
+            Assert.AreEqual(fives(myArr), new [] { 5, 10, 15 });
+        }
+    }
+}
