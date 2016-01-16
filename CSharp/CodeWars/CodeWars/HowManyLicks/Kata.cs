f629e6ad1d240c0f771f506d20855dff54e1043d
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-01-16 16:22:45 +0100

    how many licks

---
+++ CSharp/CodeWars/CodeWars/HowManyLicks/Kata.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars.HowManyLicks
+{
+    public class Kata {
+        public string TotalLicks(Dictionary<string, int> environmentVariables)
+        {
+            var defaultNumberOfLicks = 252;
+            var actualNumberOfLicks = defaultNumberOfLicks + environmentVariables.Values.Sum();
+            var baseMessage = $"It took {actualNumberOfLicks} licks to get to the tootsie roll center of a tootsie pop.";
+
+            var challenges = environmentVariables.Where(kvp => kvp.Value > 0).ToList();
+            if (!challenges.Any())
+            {
+                return baseMessage;
+            }
+
+            var toughestChallenge = challenges.OrderByDescending(kvp => kvp.Value).First().Key;
+            var toughestChallengeMessage = $"The toughest challenge was {toughestChallenge}.";
+            return $"{baseMessage} {toughestChallengeMessage}";
+        }
+    }
+}
---
+++ CSharp/CodeWars/CodeWars/HowManyLicks/TEsts.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace CodeWars.HowManyLicks
+{
+    [TestFixture]
+    public class Tests
+    {
+        private Kata kata = new Kata();
+
+        [Test]
+        public void _0_BasicTests()
+        {
+            Assert.AreEqual(
+                "It took 260 licks to get to the tootsie roll center of a tootsie pop. The toughest challenge was freezing temps.",
+                kata.TotalLicks(new Dictionary<string, int> { { "freezing temps", 10 }, { "clear skies", -2 } }));
+
+            Assert.AreEqual(
+                "It took 245 licks to get to the tootsie roll center of a tootsie pop.", 
+                kata.TotalLicks(new Dictionary<string, int> { { "happiness", -5 }, { "clear skies", -2 } }));
+
+            Assert.AreEqual(
+                "It took 252 licks to get to the tootsie roll center of a tootsie pop.", 
+                kata.TotalLicks(new Dictionary<string, int>()));
+
+            Assert.AreEqual(
+                "It took 512 licks to get to the tootsie roll center of a tootsie pop. The toughest challenge was evil wizards.", 
+                kata.TotalLicks(new Dictionary<string, int>() { { "dragons", 100 }, { "evil wizards", 110 }, { "trolls", 50 } }));
+
+            Assert.AreEqual(
+                "It took 2 licks to get to the tootsie roll center of a tootsie pop.", 
+                kata.TotalLicks(new Dictionary<string, int>() { { "white magic", -250 } }));
+        }
+    }
+}
