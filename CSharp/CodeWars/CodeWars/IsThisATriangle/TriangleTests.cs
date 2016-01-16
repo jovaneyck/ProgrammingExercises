484aeca6acee76b80a30571e234afeff227682f8
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-01-16 16:59:37 +0100

    is this a triangle

---
+++ CSharp/CodeWars/CodeWars/IsThisATriangle/TriangleTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.IsThisATriangle
+{
+    [TestFixture]
+    public class TriangleTests
+    {
+        [Test]
+        public void IsTriangle_ValidPostiveNumbers_ReturnsTrue()
+        {
+            Assert.IsTrue(Triangle.IsTriangle(5, 7, 10));
+        }
+    }
+
+    public class Triangle
+    {
+        public static bool IsTriangle(int a, int b, int c)
+        {
+            return
+                a + b > c
+                && b + c > a
+                && a + c > b;
+        }
+    }
+}
