bd511b41785cda226d52462bc88953e463af2cc8
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-02-12 10:43:42 +0100

    Got a basic algorithm working but when submitting: 1) goes out of memory 2) sometimes runs longer than the limit of 6 seconds.
    OPTIMIZE OPTIMIZE!

---
+++ CSharp/CodeWars/CodeWars.FSharp/primefactorization.fsx
@@ -0,0 +1,25 @@
+﻿(*
+Sieve of Eratosthenes:
+To find all the prime numbers less than or equal to a given integer n by Eratosthenes' method:
+
+Create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n).
+Initially, let p equal 2, the smallest prime number.
+Enumerate the multiples of p by counting to n from 2p in increments of p, and mark them in the list (these will be 2p, 3p, 4p, ... ; the p itself should not be marked).
+Find the first number greater than p in the list that is not marked. If there was no such number, stop. Otherwise, let p now equal this new number (which is the next prime), and repeat from step 3.
+*)
+
+let filterMultiples n candidates = 
+    candidates |> List.filter (fun el -> el % n <> 0)
+
+let rec findPrimesRec acc candidates =
+    match candidates with
+    | [] -> acc
+    | nextPrime :: t ->
+        findPrimesRec (nextPrime :: acc) (candidates |> filterMultiples nextPrime)
+
+let findPrimesSmallerOrEqual n = 
+    let possiblePrimes = [2..n]
+    let smallestPrime = 2
+    
+    findPrimesRec [smallestPrime] (possiblePrimes |> filterMultiples smallestPrime)
+
---
+++ CSharp/CodeWars/CodeWars/PrimesInNumbers/PrimeDecompTests.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using NUnit.Framework;
+
+namespace CodeWars.PrimesInNumbers
+{
+    [TestFixture]
+    public class PrimeDecompTests
+    {
+        [TestCase(2, new [] { 2 })]
+        [TestCase(4, new [] { 2, 3 })]
+        [TestCase(7, new [] { 2,3,5,7 })]
+        [TestCase(8, new [] { 2,3,5,7 })]
+        public void CanFindAllPrimeNumbers(int n, int [] expectedPrimes)
+        {
+            Assert.AreEqual(expectedPrimes, PrimeDecomp.PrimesSmallerOrEqualThan(n).ToArray());
+        }
+
+        [Test]
+        public void LazilyCalculatesNeededPrimes()
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            Assert.NotNull(PrimeDecomp.PrimesSmallerOrEqualThan(7775460).Take(50).ToList());
+
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
+            Assert.True(stopwatch.ElapsedMilliseconds < 3000);
+        }
+
+        [TestCase(2,"(2)")]
+        [TestCase(4,"(2**2)")]
+        [TestCase(12,"(2**2)(3)")]
+        [TestCase(7775460, "(2**2)(3**3)(5)(7)(11**2)(17)")] //Acceptance test
+        public void DecomposesANumberInPrimeFactors(int number, string expectedDecomposition)
+        {
+            Assert.AreEqual(expectedDecomposition, PrimeDecomp.factors(number));
+        }
+
+        [TestCase(2, new[] {2})]
+        [TestCase(3, new[] {3})]
+        [TestCase(6, new[] {3,2})]
+        [TestCase(10, new[] {5,2})]
+        [TestCase(12, new[] {3,2,2})]
+        public void DecomposesANumberInPrimeFactors(int number, int[] expected)
+        {
+            Assert.AreEqual(expected, PrimeDecomp.FactorDecomposition(number));
+        }
+
+        [TestCase(2, true)]
+        [TestCase(3, true)]
+        [TestCase(5, true)]
+        [TestCase(7, true)]
+        [TestCase(1, false)]
+        [TestCase(4, false)]
+        [TestCase(8, false)]
+        [TestCase(9, false)]
+        public void RecognizesPrimes(int number, bool isPrime)
+        {
+            Assert.AreEqual(isPrime, PrimeDecomp.IsPrime(number));
+        }
+    }
+
+    public class PrimeDecomp
+    {
+        public static string factors(int number)
+        {
+            return
+                string.Join(
+                    "",
+                    FactorDecomposition(number)
+                        .GroupBy(n => n)
+                        .OrderBy(g => g.Key)
+                        .Select(n => new {factor = n.Key, occurences = n.Count()})
+                        .Select(f =>
+                            f.occurences == 1 
+                                ? $"({f.factor})" 
+                                : $"({f.factor}**{f.occurences})"));
+
+        }
+
+        public static IList<int> FactorDecomposition(int number)
+        {
+            if (IsPrime(number))
+            {
+                return new List<int> {number};
+            }
+
+            var possibleFactors = PrimesSmallerOrEqualThan(number);
+
+            foreach (var f in possibleFactors)
+            {
+                if (number % f == 0)
+                {
+                    var result = FactorDecomposition(number / f);
+                    result.Add(f);
+                    return result;
+                }
+            }
+
+            return new List<int>();
+        }
+
+        public static bool IsPrime(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+
+            return Enumerable.Range(2, number-2).All(n => number % n != 0);
+        }
+
+        /*
+            Sieve of Eratosthenes:
+            To find all the prime numbers less than or equal to a given integer n by Eratosthenes' method:
+
+            Create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n).
+            Initially, let p equal 2, the smallest prime number.
+            Enumerate the multiples of p by counting to n from 2p in increments of p, and mark them in the list (these will be 2p, 3p, 4p, ... ; the p itself should not be marked).
+            Find the first number greater than p in the list that is not marked. If there was no such number, stop. Otherwise, let p now equal this new number (which is the next prime), and repeat from step 3.
+        */
+        public static IEnumerable<int> PrimesSmallerOrEqualThan(int number)
+        {
+            var possiblePrimes =
+                Enumerable
+                    .Range(2, number - 1)
+                    .ToList();
+
+            var smallestPrime = 2;
+            
+            while (smallestPrime != default(int))
+            {
+                yield return smallestPrime;
+
+                possiblePrimes = possiblePrimes.Where(pp => pp % smallestPrime != 0).ToList();
+                smallestPrime = possiblePrimes.FirstOrDefault();
+            }
+        }
+    }
+}
