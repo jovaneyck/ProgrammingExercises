a58bf34444853cbdb8030bed8bc19cd7677cd725
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-03-24 14:48:13 +0100

    added basic property based testing example

---
+++ FSharp/FSharpLearning/PropertyBasedTesting/ExampleTests.fs
@@ -0,0 +1,14 @@
+﻿module Exampletests
+
+open Xunit
+open Swensen.Unquote
+open FsCheck
+open FsCheck.Xunit
+
+[<Fact>]
+let sanityCheck() = 
+    test <@ 1  + 1 = 2 @>
+
+[<Property>]
+let VerifiesAProperty (xs : int list) = 
+   xs = (xs |> List.rev |> List.rev)
