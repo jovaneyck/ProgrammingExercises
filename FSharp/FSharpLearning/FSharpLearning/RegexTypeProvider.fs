f3142602f98a13a67bcb71335e321fcc2357bef2
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-03-30 17:42:15 +0200

    added regex type provider example

---
+++ FSharp/FSharpLearning/FSharpLearning/RegexTypeProvider.fs
@@ -0,0 +1,18 @@
+﻿module RegexTypeProvider
+
+open Xunit
+open Swensen.Unquote
+open FSharp.Text.RegexProvider
+
+type ContactInfoRegex = Regex< @"(?<Name>[A-Za-z]*);(?<TeleponeNumber>\d*);" >
+let myMatcher = ContactInfoRegex()
+
+[<Fact>]
+let canAccessRegexGroupsThroughTypeCheckPropertiesYay() =
+    let aMatch = 
+        match myMatcher.Match("Bob;0498736277;") with
+        | regex when regex.Success-> 
+            Some (regex.Name.Value, regex.TeleponeNumber.Value) //Static typechecking on regex groups!
+        | _ -> 
+            None
+    test <@ Some ("Bob", "0498736277") = aMatch @>
