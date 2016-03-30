47498696fb00d2b003a4e663f5276172f3ecaed4
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-03-30 17:22:52 +0200

    Added some examples of measures, query expressions and csv type provider

---
+++ FSharp/FSharpLearning/FSharpLearning/Measures.fs
@@ -0,0 +1,21 @@
+﻿module Measures
+
+open Swensen.Unquote
+open Xunit
+
+[<Measure>] type m
+[<Measure>] type h
+
+//Can create derived measure formulas
+[<Measure>] type mph = m / h
+
+//Can enforce measures as argument types
+let toMilesPerHour (miles : float<m>) (hour : float<h>) : float<mph> = miles / hour
+
+[<Fact>]
+let measuresCanResultInCompilerErrorsWhenDevelopersMakeAMistake() =
+    let distance = 50.0<m>
+    let time = 2.0<h>
+
+    test <@ 25.0<mph> = (toMilesPerHour distance time) @>
+    //test <@ 25.0<mph> = (toMilesPerHour time distance) @> //Compiler errors! Type mismatch: expecting a float<m> but given a float<h>!
---
+++ FSharp/FSharpLearning/FSharpLearning/QueryExpressions.fs
@@ -0,0 +1,31 @@
+﻿module QueryExpressions
+
+open Swensen.Unquote
+open Xunit
+open System.Linq
+
+[<Fact>]
+let canUseStandardLinqQueries() =
+    let filtered = 
+        [1..100]
+            .Where(fun el -> el % 2 = 0)
+            .Skip(1)
+            .Take(2)
+            .Select(fun el -> el + 1)
+            .ToList()
+        |> List.ofSeq
+
+    test <@ [5;7] = filtered @>
+
+[<Fact>]
+let canUseQueryExpressionsToGiveThingsAMoreFunctionalFeel() =
+    let filtered = 
+        query {
+            for i in [1..100] do
+            where (i % 2 = 0)
+            skip 1
+            take 2
+            select (i + 1)
+        } |> List.ofSeq
+
+    test <@ [5;7] = filtered @>
