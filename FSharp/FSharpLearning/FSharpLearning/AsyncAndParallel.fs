746eb11e55d00b810702593a37395497c3928c5d
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-03-31 08:42:56 +0200

    added some examples of using .NET TPL

---
+++ FSharp/FSharpLearning/FSharpLearning/AsyncAndParallel.fs
@@ -0,0 +1,13 @@
+﻿module AsyncAndParallel
+
+open Xunit
+open Swensen.Unquote
+open System.Threading.Tasks
+open System.Text
+
+//TPL = _potential_ parallellism (tasks instead of threads!)
+[<Fact>]
+let canUseTPLDataParallellism() =
+    let buffer = new StringBuilder()
+    let result = Parallel.ForEach([1..10000], fun (i : int) -> buffer.Append(i) |> ignore)
+    test <@ buffer.ToString().Contains("1337") @>
--- FSharp/FSharpLearning/PropertyBasedTesting/ExampleTests.fs
+++ FSharp/FSharpLearning/PropertyBasedTesting/ExampleTests.fs
@@ -5,10 +5,16 @@ open Swensen.Unquote
 open FsCheck
 open FsCheck.Xunit
 
-[<Fact>]
-let sanityCheck() = 
-    test <@ 1  + 1 = 2 @>
-
 [<Property>]
-let ``reverse of reverse of a list is the list itself`` (xs : int list) = 
-   xs = (xs |> List.rev |> List.rev)
+let ``reverse of reverse of a list is the list itself`` (xs : int list) (* generator supplies random input *)= 
+   xs = (xs |> List.rev |> List.rev) 
+   //If a failure occurs, tries to shrink to smallest input that falsifies the property
+   (*
+    FsCheck.Xunit.PropertyFailedException
+
+    Falsifiable, after 3 tests (3 shrinks) (StdGen (455753529,296137357)):
+    Original:
+    [-1; -3; 2; 0]
+    Shrunk:
+    [1; 0]
+   *)
