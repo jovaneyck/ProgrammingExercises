3333a2dbcece89a27da99f860f52b1572d13c6b8
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-04-01 15:47:52 +0200

    Added some async computation expressions and Actor-based async examples. Wow.

--- FSharp/FSharpLearning/FSharpLearning/AsyncAndParallel.fs
+++ FSharp/FSharpLearning/FSharpLearning/AsyncAndParallel.fs
@@ -40,3 +40,61 @@ let canUseTasks() =
         .Wait() //aaand wait for complete result
 
     test <@ buffer.ToString() = "continued after 1337" @>
+
+//So far for the boring .NET stuff. Let's get FUNCTIONAL!
+[<Fact>]
+let canUseAsyncComputationExpressions() = 
+    let buffer = new StringBuilder()
+
+    //Looks confusing? Check out Computation Expressions!
+    async {
+        do buffer.Append("1") |> ignore
+        do buffer.Append("2") |> ignore
+        do! Async.Sleep 10
+        do buffer.Append("3") |> ignore
+    } |> Async.RunSynchronously
+
+    test <@ buffer.ToString().Contains("3") @>
+
+//Heard of Akka, agent-model for async programming? FSharp has that out of the box!
+type Agent<'T> = MailboxProcessor<'T>
+type Message = | Message of string
+
+[<Fact>]
+let canUseActorModelOutOfTheBox() =
+    let buffer = new StringBuilder()
+    let agent = 
+        Agent<Message>.Start(fun inbox -> 
+            let rec loop () =
+                async {
+                    let! (Message(body)) = inbox.Receive()
+                    buffer.Append(body) |> ignore
+                    return! loop()
+                }
+            loop())
+
+    agent.Post(Message "What the hell ")
+    agent.Post(Message "World")
+
+    //Wait for the actor to process its inbox entirely
+    //NEVER EVER EVER do this in prod code kthx
+    while agent.CurrentQueueLength <> 0 do
+        System.Threading.Thread.Sleep 1
+
+    test <@ buffer.ToString() = "What the hell World" @>
+
+type ReplyMessage = | ReplyMessage of Message * AsyncReplyChannel<string>
+
+[<Fact>]
+let canReceiveRepliesFromActors() = 
+    let agent = Agent.Start(fun inbox ->
+        let rec loop() =
+            async {
+                let! (ReplyMessage(Message(message), replyChannel)) = inbox.Receive()
+                replyChannel.Reply(sprintf "Echo: %s" message)
+                return! loop()
+            }
+        loop())
+
+    let response = agent.PostAndReply(fun replyChannel -> ReplyMessage(Message("Heya!"), replyChannel))
+    test <@ response = "Echo: Heya!" @>
