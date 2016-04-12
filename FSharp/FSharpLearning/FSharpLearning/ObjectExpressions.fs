e90cfa0606fd78ff7ba4269711f2cba702159598
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-04-12 09:45:18 +0200

    Added decompiled code of object expressions

--- FSharp/FSharpLearning/FSharpLearning/ObjectExpressions.fs
+++ FSharp/FSharpLearning/FSharpLearning/ObjectExpressions.fs
@@ -3,15 +3,22 @@
 open Xunit
 open Swensen.Unquote
 
-type Customer = { Name: string; NumberOfProductsOrdered: int }
+type Order = {Id : int}
+type Customer = { Name: string; NumberOfProductsOrdered: int; Orders : Order list }
 
 [<Fact>]
 let canCopyAValueAndModifySomePropertiesUsingTheWithKeyword() =
-    let john = {Name="John"; NumberOfProductsOrdered=0}
+    let john = {Name="John"; NumberOfProductsOrdered=0; Orders = [] }
+    let johnAfterOrderingSomething = { john with NumberOfProductsOrdered = 1; Orders= [ {Id = 123} ] }
+    let johnAfterARename = {johnAfterOrderingSomething with Name = "Johnny"}
+    test <@ johnAfterARename = {Name="Johnny"; NumberOfProductsOrdered=1; Orders = [ {Id = 123} ] } @>
 
-    let afterOrdering = {john with NumberOfProductsOrdered = 2}
-
-    test <@ afterOrdering = {Name="John"; NumberOfProductsOrdered=2} @>
+    //Worried about performance hit when "copying" values? Don't. Code above compiles to:
+    (*
+        ObjectExpressions.Customer func1 = new ObjectExpressions.Customer("John", 0, []);
+        ObjectExpressions.Customer func2 = new ObjectExpressions.Customer(func1.Name, 1, [new ObjectExpressions.Order(123)]);
+        ObjectExpressions.Customer func3 = new ObjectExpressions.Customer("Johnny", customer.NumberOfProductsOrdered, customer.Orders);
+    *)
 
 type IPrintable =
     abstract member print : unit -> string
