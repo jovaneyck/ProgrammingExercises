293a8e3226bf58c1e76fe6203794c15cfad9c87b
prGmtc <jo.vaneyck@gmail.com>
2016-05-08 08:57:16 +0200

    add a simple UpperCaseLetters Arbitrary

--- FSharp/DiamondKata/PBTDiamondKata/DiamondProperties.fs
+++ FSharp/DiamondKata/PBTDiamondKata/DiamondProperties.fs
@@ -1,9 +1,15 @@
 ﻿module DiamondProperties
 
 open System
+open FsCheck
 open FsCheck.Xunit
 
-[<Property>]
+type UppercaseLetters =
+    static member Char() =
+        Arb.Default.Char()
+        |> Arb.filter (fun c -> 'A' <= c && c <= 'Z')
+
+[<Property(Arbitrary = [| typeof<UppercaseLetters> |])>]
 let ``Diamond is non-empty`` (letter : char) =
     let actual = Diamond.make letter
     not (String.IsNullOrWhiteSpace actual)
