5f1f3b7b62ff343a29f6cb03e416576a78efdb64
prGmtc <jo.vaneyck@gmail.com>
2016-05-08 07:58:26 +0200

    Set up project with dependencies and a first property

---
+++ FSharp/DiamondKata/PBTDiamondKata/DiamondProperties.fs
@@ -0,0 +1,9 @@
+﻿module DiamondProperties
+
+open System
+open FsCheck.Xunit
+
+[<Property>]
+let ``Diamond is non-empty`` (letter : char) =
+    let actual = Diamond.make letter
+    not (String.IsNullOrWhiteSpace actual)
