e165da1e06155e1e2389ef9473ddf6383678aeaf
prGmtc <jo.vaneyck@gmail.com>
2016-05-08 11:41:21 +0200

    refactor domain logic a bit

--- FSharp/DiamondKata/PBTDiamondKata/Diamond.fs
+++ FSharp/DiamondKata/PBTDiamondKata/Diamond.fs
@@ -2,28 +2,27 @@
 
 open System
 
-let make letter = 
+let make letter =
+    let mirrorAndFuse ls =
+        ls @ (ls |> List.rev |> List.tail)  
     let makeLine width (letter, letterIndex) =
         let leftPadding = String(' ', width - 1 - letterIndex)
         let innerPadding = String(' ', width - 1 - leftPadding.Length)
-        match letter with
-        | 'A' ->
-            sprintf "%s%c%s" leftPadding letter leftPadding
-        | other ->
-            let left = 
-                sprintf "%s%c%s" leftPadding letter innerPadding
-                |> Seq.toList
-            left
-            @ (left |> List.rev |> List.tail)
-            |> List.map string
-            |> List.reduce (sprintf "%s%s")
+        let left = 
+            sprintf "%s%c%s" leftPadding letter innerPadding
+            |> Seq.toList
+
+        left
+        |> mirrorAndFuse
+        |> List.map string
+        |> List.reduce (sprintf "%s%s")
 
     let lettersWithTheirIndex = 
         ['A'..letter]
         |> List.mapi(fun i l -> l,i)
 
     lettersWithTheirIndex
-    @ (lettersWithTheirIndex |> List.rev |> List.tail )
+    |> mirrorAndFuse
     |> Seq.map (makeLine lettersWithTheirIndex.Length)
     |> Seq.reduce (fun f s -> 
         sprintf "%s%s%s" f Environment.NewLine s) 
