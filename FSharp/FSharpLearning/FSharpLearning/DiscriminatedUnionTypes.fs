8425a7b9e5187276b1d077cc1007a57c2acbb8ee
prGmtc <jo.vaneyck@gmail.com>
2016-05-09 20:51:36 +0200

    added some more examples on discriminated unions, functions, option type

---
+++ FSharp/FSharpLearning/FSharpLearning/DiscriminatedUnionTypes.fs
@@ -0,0 +1,26 @@
+﻿module DiscriminatedUnionTypes
+
+open Xunit
+open Swensen.Unquote
+
+type Tree =
+    | InnerNode of int * Tree * Tree
+    | Leaf of int
+
+let rec NbNodes t =
+    match t with
+    | Leaf _ -> 1
+
+[<Fact>]
+let canPatternMatchOnUnionTypes() =
+    let tree = 
+        InnerNode(3, 
+            InnerNode(2, 
+                Leaf 1, 
+                Leaf 1), 
+            Leaf 2)
+
+    let tuple = (1 ,2 ,3)
+    let (x,_,z) = tuple
+
+    test <@ 5 = NbNodes tree @>
---
+++ FSharp/FSharpLearning/FSharpLearning/Options.fs
@@ -0,0 +1,21 @@
+﻿module Options
+
+let customers = 
+    [(1, "John"); (2, "Alice")]
+    |> Map.ofList
+
+type Option<'a> =
+    | Some of 'a
+    | Nothing
+
+let getCustomer id =
+    if not (customers |> Map.containsKey id) then
+        Nothing
+    else
+        let c = customers |> Map.find id
+        Some c
+
+let print c =
+    match c with
+    | Some name -> printfn "%s" name
+    | Nothing -> printfn "No customer found"
