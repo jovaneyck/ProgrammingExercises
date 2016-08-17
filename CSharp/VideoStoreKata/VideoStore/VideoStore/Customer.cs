93c66cc5d0a24c0ad723b5c6e5017e9005937f21
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-08-17 10:23:24 +0200

    string interpolation + foreach instead of literal enumerator

--- CSharp/VideoStoreKata/VideoStore/VideoStore/Customer.cs
+++ CSharp/VideoStoreKata/VideoStore/VideoStore/Customer.cs
@@ -21,29 +21,27 @@ namespace VideoStore
         {
             double totalAmount = 0;
             var frequentRenterPoints = 0;
-            var rentals = _rentals.GetEnumerator();
             var result = "Rental Record for " + Name + "\n";
 
-            while (rentals.MoveNext())
+            foreach (var rental in _rentals)
             {
                 double thisAmount = 0;
-                var each = rentals.Current;
 
                 // determines the amount for each line
-                switch (each.Movie.PriceCode)
+                switch (rental.Movie.PriceCode)
                 {
                     case Movie.REGULAR:
                         thisAmount += 2;
-                        if (each.DaysRented > 2)
-                            thisAmount += (each.DaysRented - 2)*1.5;
+                        if (rental.DaysRented > 2)
+                            thisAmount += (rental.DaysRented - 2)*1.5;
                         break;
                     case Movie.NEW_RELEASE:
-                        thisAmount += each.DaysRented*3;
+                        thisAmount += rental.DaysRented*3;
                         break;
                     case Movie.CHILDRENS:
                         thisAmount += 1.5;
-                        if (each.DaysRented > 3)
-                            thisAmount += (each.DaysRented - 3)*1.5;
+                        if (rental.DaysRented > 3)
+                            thisAmount += (rental.DaysRented - 3)*1.5;
                         break;
                 }
 
@@ -51,17 +49,17 @@ namespace VideoStore
                 frequentRenterPoints++;
 
                 // add bonus for a two day new release rental
-                if (each.Movie.PriceCode == Movie.NEW_RELEASE && each.DaysRented > 1)
+                if (rental.Movie.PriceCode == Movie.NEW_RELEASE && rental.DaysRented > 1)
                     frequentRenterPoints++;
 
                 // show figures for this rental
-                result += "\t" + each.Movie.Title + "\t" + $"{thisAmount:F1}" + "\n";
+                result += $"\t{rental.Movie.Title}\t{thisAmount:F1}\n";
                 totalAmount += thisAmount;
             }
 
             // add footer lines
-            result += "You owed " + $"{totalAmount:F1}" + "\n";
-            result += "You earned " + frequentRenterPoints + " frequent renter points";
+            result += $"You owed {totalAmount:F1}\n";
+            result += $"You earned {frequentRenterPoints} frequent renter points";
 
             return result;
         }
