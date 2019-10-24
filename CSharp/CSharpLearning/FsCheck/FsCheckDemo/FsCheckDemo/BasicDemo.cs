f4ba548942b7473fc4ffa0a78d6e69d6d7aea2c9
Jo Van Eyck <jo.vaneyck@ae.be>
2019-10-24 16:59:24 +0200

    [fscheck] basic xunit+c# example

---
+++ CSharp/CSharpLearning/FsCheck/FsCheckDemo/FsCheckDemo/BasicDemo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FsCheck;
+using Xunit;
+
+namespace FsCheckDemo
+{
+    public class BasicDemo
+    {
+        [Fact]
+        public void Verifying_A_Property()
+        {
+            Prop
+                .ForAll(Arb.From<int[]>(), xs => xs.Reverse().Reverse().SequenceEqual(xs))
+                .QuickCheckThrowOnFailure();
+        }
+    }
+}
